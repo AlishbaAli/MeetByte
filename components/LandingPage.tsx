23d681eab48e23f8bb74022459fe23dc37ac17a7
Alishba Ali <alishba5ahmedali@gmail.com>
2025-07-08 00:07:03 -0400

    db connected using drizzle

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -2,9 +2,6 @@
 import { SignIn } from "@clerk/nextjs";
 import { shadesOfPurple } from "@clerk/themes";
 import Image from "next/image";
-
-// Marks this file as a Client Component
-
 export default function LandingPage() {
   return (
     <main className="flex items-center p-40 gap-24 animate-fade-in max-md:flex-col">
---
+++ drizzle.config.ts
@@ -0,0 +1,25 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+import { defineConfig } from "drizzle-kit";
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not defined in environment variables.");
+}
+
+export default defineConfig({
+  schema: "./drizzle/schema.ts",
+
+  out: "./drizzle/migrations",
+
+  dialect: "postgresql",
+
+  strict: true,
+
+  verbose: true,
+
+  dbCredentials: {
+    url: databaseUrl,
+  },
+});
